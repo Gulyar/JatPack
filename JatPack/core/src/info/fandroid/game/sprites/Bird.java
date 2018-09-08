1a7560bae8f9d608a9c339c2cac6b77b999731f3
Oleg <Olegarsion2018@gmail.com>
2018-09-08 21:58:53 +0300

    I wote class Bird

---
+++ JatPack/core/src/info/fandroid/game/sprites/Bird.java
@@ -0,0 +1,49 @@
+package info.fandroid.game.sprites;
+
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.math.Vector3;
+
+public class Bird {
+    public static final int GRAVITY = -15;
+    private Vector3 position;
+    private Vector3 velosity;
+
+    private Texture bird;
+
+    public Bird(int x, int y)
+    {
+        position = new Vector3(x,y,0);
+        velosity = new Vector3(0,0,0);
+        bird = new Texture("bird.jpg");
+    }
+
+    public Vector3 getPosition() {
+        return position;
+    }
+
+    public Texture getBird() {
+        return bird;
+    }
+
+    public void update(float dt){
+        if(position.y > 0)
+        {
+            velosity.add(0,GRAVITY,0);
+        }
+        velosity.scl(dt);
+        position.add(0,velosity.y,0);
+
+        if(position.y < 0)
+        {
+            position.y = 0;
+        }
+
+        velosity.scl(1/dt);
+
+    }
+
+    public void jump()
+    {
+        velosity.y = 300;
+    }
+}
